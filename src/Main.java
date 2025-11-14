334164485b317a0f72cf62ac9f77951393042f37
vladimirbroida <vboirda@gmail.com>
2025-11-14 21:05:09 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 1;
+        int clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
